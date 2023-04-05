1d48dd58ac7f3b2f7e615187fc00bcff95169c03
Zulu <126271621+markf635@users.noreply.github.com>
2023-04-05 15:42:26 -0400

    Update MQTT.py

--- MQTT.py
+++ MQTT.py
@@ -1,7 +1,7 @@
 def connectMQTT():
     # replace HiveMQ server url, user, and password with own
-    client = MQTTClient(client_id=b"Alan_PicoW", server=b"********.hivemq.cloud", port=0, user=b"********", password=b"********",
-                        keepalive=7200, ssl=True, ssl_params={'server_hostname':'********.hivemq.cloud'}
+    client = MQTTClient(client_id=b"Alan_PicoW", server=b"cluster_URL.hivemq.cloud", port=0, user=b"MQTT_username", password=b"MQTT_password",
+                        keepalive=7200, ssl=True, ssl_params={'server_hostname':'cluster_URL.hivemq.cloud'}
                         )
     client.connect()
     return client
