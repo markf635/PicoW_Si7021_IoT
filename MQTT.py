d9f314a81faf516505d9acca9b6d690f27ac8644
Zulu <126271621+markf635@users.noreply.github.com>
2023-04-18 14:44:22 -0400

    Delete MQTT.py

--- MQTT.py
+++
@@ -1,7 +0,0 @@
-def connectMQTT():
-    # replace HiveMQ server url, user, and password with own
-    client = MQTTClient(client_id=b"Alan_PicoW", server=b"cluster_URL.hivemq.cloud", port=0, user=b"MQTT_username", password=b"MQTT_password",
-                        keepalive=7200, ssl=True, ssl_params={'server_hostname':'cluster_URL.hivemq.cloud'}
-                        )
-    client.connect()
-    return client
